2b5deb5fe7f4bbe10c94b0036737385070323554
Artur Petriv <artur.petriv@gmail.com>
2020-09-15 14:22:24 +0300

    Create basic template of whatsapp app

---
+++ whatsapp/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Whatsapp App</h1>
+      <div className="app__body">
+        {/* Sidebar */}
+        {/* Chat */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
